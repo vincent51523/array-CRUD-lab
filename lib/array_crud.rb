fd2dfdd53fd953f539869b5e5082db467ba10ac5
bin <vincent418@icloud.com>
2021-03-06 21:58:03 -0600

    commit tree 12ebd435d1c54094267c9583f4ff3fc0c34da0a9

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,36 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+
+  ["1", "2", "3", "4" ]
 end
 
-def add_element_to_end_of_array(array, element)
-  
+def add_element_to_end_of_array(xyz, el)
+  xyz.push(el)
 end
 
-def add_element_to_start_of_array(array, element)
-  
+def add_element_to_start_of_array(cat, dog)
+  cat.unshift(dog)
 end
 
 def remove_element_from_end_of_array(array)
-  
+  array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  array[index_number]
 end
 
 def retrieve_first_element_from_array(array)
-  
+    array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+    array[array.length - 1]
 end
